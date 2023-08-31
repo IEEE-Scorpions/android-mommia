9fea86131ba977091531df9072f39af1a5deb43f
MohamedAhmed5916 <120657300+Mohamed-5der@users.noreply.github.com>
2023-08-31 13:35:31 +0300

    splash screen

--- app/src/main/java/com/example/mommia/MainActivity.java
+++ app/src/main/java/com/example/mommia/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.mommia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+import com.agrawalsuneet.dotsloader.loaders.LinearDotsLoader;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    ProgressBar pb;
+    int count = 0;
+    Handler handler = new Handler();
+    private Runnable stopProgressRunnable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        LinearDotsLoader containerLL =  findViewById(R.id.dotsLoader);
+
+        LinearDotsLoader loader = new LinearDotsLoader(this);
+        loader.setDefaultColor(ContextCompat.getColor(this, R.color.Opacity));
+        loader.setSelectedColor(ContextCompat.getColor(this, R.color.white));
+        loader.setSingleDir(true);
+        loader.setNoOfDots(5);
+        loader.setSelRadius(40);
+        loader.setExpandOnSelect(true);
+        loader.setRadius(30);
+        loader.setDotsDistance(20);
+        loader.setAnimDur(500);
+        loader.setShowRunningShadow(true);
+        loader.setFirstShadowColor(ContextCompat.getColor(this, R.color.yellow));
+        loader.setSecondShadowColor(ContextCompat.getColor(this, R.color.yellow));
+
+        //containerLL.addView(loader);
+
+
     }
+
 }
