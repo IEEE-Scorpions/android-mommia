f0c43f48568513aecb37e039ea7fbe5916d3e369
MohamedAhmed5916 <120657300+Mohamed-5der@users.noreply.github.com>
2023-08-31 16:33:35 +0300

    register

--- app/src/main/java/com/example/mommia/MainActivity.java
+++ app/src/main/java/com/example/mommia/MainActivity.java
@@ -3,25 +3,28 @@ package com.example.mommia;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.agrawalsuneet.dotsloader.loaders.LinearDotsLoader;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
+import java.lang.reflect.Array;
 import java.sql.Time;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar pb;
-    int count = 0;
-    Handler handler = new Handler();
-    private Runnable stopProgressRunnable;
 
 
     @Override
@@ -31,24 +34,32 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        LinearDotsLoader containerLL =  findViewById(R.id.dotsLoader);
+        showContinueAsDialog();
 
-        LinearDotsLoader loader = new LinearDotsLoader(this);
-        loader.setDefaultColor(ContextCompat.getColor(this, R.color.Opacity));
-        loader.setSelectedColor(ContextCompat.getColor(this, R.color.white));
-        loader.setSingleDir(true);
-        loader.setNoOfDots(5);
-        loader.setSelRadius(40);
-        loader.setExpandOnSelect(true);
-        loader.setRadius(30);
-        loader.setDotsDistance(20);
-        loader.setAnimDur(500);
-        loader.setShowRunningShadow(true);
-        loader.setFirstShadowColor(ContextCompat.getColor(this, R.color.yellow));
-        loader.setSecondShadowColor(ContextCompat.getColor(this, R.color.yellow));
+    }
 
-        //containerLL.addView(loader);
-
+    public void showContinueAsDialog(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setCancelable(false);
+                dialog.setContentView(R.layout.continue_as);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                dialog.show();
+                Button tourist =dialog.findViewById(R.id.btnTourist);
+                Button TourGuide =dialog.findViewById(R.id.btnTorGuide);
+                tourist.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent=new Intent(MainActivity.this,Register.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+            }
+        }, 2000);
 
     }
 
