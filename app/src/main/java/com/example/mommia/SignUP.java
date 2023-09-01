8e5a83753a71fb9559d1c715e21134b59535be92
Ahmed emad <ahmed.emad.23112003@gmail.com>
2023-09-01 21:42:43 +0300

    update sign up screen

--- app/src/main/java/com/example/mommia/SignUP.java
+++ app/src/main/java/com/example/mommia/SignUP.java
@@ -1,14 +1,30 @@
 package com.example.mommia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUP extends AppCompatActivity {
+    EditText et_full_name, et_email, et_password, et_phone_number, et_national, et_date_of_birth, et_passport_national;
+    SwitchCompat sw_gender;
+    Button btn_sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        et_full_name = findViewById(R.id.et_full_name);
+        et_email = findViewById(R.id.et_email);
+        et_password = findViewById(R.id.et_password);
+        et_phone_number = findViewById(R.id.et_phone_number);
+        sw_gender = findViewById(R.id.sw_gender);
+        et_national = findViewById(R.id.et_national);
+        et_date_of_birth = findViewById(R.id.et_date_of_birth);
+        et_passport_national = findViewById(R.id.et_passport_national);
+        btn_sign_up = findViewById(R.id.btn_sign_up);
     }
 }
