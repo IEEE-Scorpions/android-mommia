5c8cb7b9533c67ffacdf5d4b081c1aeff1c9da97
MohamedAhmed5916 <120657300+Mohamed-5der@users.noreply.github.com>
2023-09-01 18:25:00 +0300

    add new data register and continue as

--- app/src/main/java/com/example/mommia/Register.java
+++ app/src/main/java/com/example/mommia/Register.java
@@ -2,15 +2,46 @@ package com.example.mommia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
-
+    Button signIn;
+    DisplayMetrics display;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+         signIn=findViewById(R.id.btn_sign_in);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showContinueAsDialog();
+
+            }
+        });
+    }
+    public void showContinueAsDialog(){
+                final Dialog dialog = new Dialog(Register.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setCancelable(false);
+                dialog.setContentView(R.layout.sign_in);
+                display=getResources().getDisplayMetrics();
+                dialog.getWindow().setLayout(display.widthPixels,(10*display.heightPixels)/10);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                dialog.show();
 
     }
+
 }
