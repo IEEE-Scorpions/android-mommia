26be4d62847b5143c82eb2981e05befe6a24a2b9
MohamedAhmed5916 <120657300+Mohamed-5der@users.noreply.github.com>
2023-09-04 02:06:28 +0300

    update

---
+++ app/src/main/java/com/example/mommia/HomeTouristActivity.java
@@ -0,0 +1,14 @@
+package com.example.mommia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeTouristActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_tourist);
+    }
+}
