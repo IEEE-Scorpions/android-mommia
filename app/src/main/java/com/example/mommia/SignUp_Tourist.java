e94cdd7804600ddddd7e96d8bbeedc1877e8881a
Ahmed emad <ahmed.emad.23112003@gmail.com>
2023-09-03 17:44:06 +0300

    sign up tourist screen

---
+++ app/src/main/java/com/example/mommia/SignUp_Tourist.java
@@ -0,0 +1,31 @@
+package com.example.mommia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignUp_Tourist extends AppCompatActivity {
+    EditText to_et_full_name, to_et_email, to_et_password, to_et_phone_number, to_et_national, to_et_date_of_birth, to_et_passport_national;
+    SwitchCompat to_sw_gender;
+    Button btn_next;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_tourist);
+
+
+        to_et_full_name = findViewById(R.id.to_et_full_name);
+        to_et_email = findViewById(R.id.to_et_email);
+        to_et_password = findViewById(R.id.to_et_password);
+        to_et_phone_number = findViewById(R.id.to_et_phone);
+        to_sw_gender = findViewById(R.id.to_sw_btn_gender);
+        to_et_national = findViewById(R.id.to_et_national);
+        to_et_date_of_birth = findViewById(R.id.to_et_date_of_birth);
+        to_et_passport_national = findViewById(R.id.to_et_passport_national);
+        btn_next = findViewById(R.id.btn_next);
+    }
+}
